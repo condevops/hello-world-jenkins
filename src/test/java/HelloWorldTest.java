1f6c1220e6f3902112636abbe96731bb2a23bd9d
Oleksii Lesiuk <olesiuk@conductor.com>
2022-12-15 15:36:27 +0200

    add simple test

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -1,9 +1,11 @@
 import org.junit.Test;
 
+import static org.junit.Assert.assertTrue;
+
 public class HelloWorldTest {
 
     @Test
     public void test() {
-        // dummy
+        assertTrue(true);
     }
 }
